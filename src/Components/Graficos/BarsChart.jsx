1a2c3b2601d57110c3fb7d3beffb58c97ad37fcc
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-01-17 09:29:32 -0600

    aqui esta bien

---
+++ src/Components/Graficos/BarsChart.jsx
@@ -0,0 +1,50 @@
+
+import React from 'react';
+//esto se importa de recharts
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+//data para mostrar en el grafico
+const data = [
+    { mes: "Enero", totalMes: 10, pagado: 60 },
+    { mes: 'Febrero', totalMes: 25, pagado: 70 },
+    { mes: 'Marzo', totalMes: 80, pagado: 65 },
+    { mes: 'Abril', totalMes: 35, pagado: 85 },
+    { mes: 'Mayo', totalMes: 12, pagado: 48 },
+    { mes: 'Junio', totalMes: 30, pagado: 69 },
+    { mes: 'Julio', totalMes: 15, pagado: 120 },
+    { mes: 'Agosto', totalMes: 15, pagado: 78 },
+    { mes: 'Septiembre', totalMes: 15, pagado: 35 },
+    { mes: 'Octubre', totalMes: 14, pagado: 78 },
+    { mes: 'Noviembre', totalMes: 40, pagado: 12 },
+    { mes: 'Diciembre', totalMes: 30, pagado: 45 },
+]
+
+
+const BarsChart = () => {
+    return (
+        <div>
+            <ResponsiveContainer width="100%" aspect={2} fill="#fff">
+                <BarChart
+                    data={data}
+                    width={500}
+                    height={300}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="4 1 2" />
+                    <XAxis dataKey="mes" color="#fff" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend fill='#fff' />
+                    <Bar dataKey="totalMes" fill="#04041b" />
+                    <Bar dataKey="pagado" fill="#4d51b3" />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
+    );
+}
+
+export default BarsChart;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import { Col, Container, Row, Button } from 'reactstrap';
+import BarsChart from '../../Components/Graficos/BarsChart';
+import Circular from '../../Components/Graficos/Circular';
 
 const Home = () => {
     return (
         <>
-        <Container>
-            <Row>
-                <Col xs="4" className="bg-primary">COLUMNA 1</Col>
-                <Col xs="4" className="bg-warning">COLUMNA 2</Col>
-                <Col xs="4" className="bg-danger">COLUMNA 3</Col>
-            </Row>
-        </Container>
+            <Container>
+                <Row>
+                    <Col xs="10">
+                        <BarsChart />
+                    </Col>
+                    <Col xs="2" className="bg-ligth">
+                        <Button color="primary" className='m-2 w-100'>
+                            primary
+                        </Button>
+                        {' '}
+                        <Button className='m-2 w-100'>
+                            secondary
+                        </Button>
+                        {' '}
+                        <Button color="success" className='m-2 w-100'>
+                            success
+                        </Button>
+                        {' '}
+                        <Button color="info" className='m-2 w-100'>
+                            info
+                        </Button></Col>
+                        
+                    <Col xs="6"  className="bg-ligth h-auto mt-4"></Col>
+                    <Col xs="4" className="bg-danger">COLUMNA 3</Col>
+                </Row>
+            </Container>
         </>
     );
 }
