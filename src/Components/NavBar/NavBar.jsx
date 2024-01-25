71a875bffa33bc95554bab7cc7014b9fec5f5c06
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-01-25 11:49:19 -0600

    aqui arrege algo

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Collapse,
     Navbar,
@@ -13,12 +13,40 @@ import {
 
 function NavBar(args) {
     const [isOpen, setIsOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
 
     const toggle = () => setIsOpen(!isOpen);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY;
+
+            // Cambia la clase del Navbar basado en la posición del scroll
+            setIsScrolled(scrollTop > 50);
+        };
+
+        // Agrega un evento de escucha para el scroll
+        window.addEventListener('scroll', handleScroll);
+
+        // Limpia el evento cuando el componente se desmonta
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []); // El segundo argumento [] asegura que este efecto solo se ejecute una vez al montar el componente
+
     return (
         <div>
-            <Navbar style={{ backgroundColor: '#ffff', }} dark expand="md">
+            <Navbar
+                style={{
+                    backgroundColor: '#ffff',
+                    position: isScrolled ? 'fixed' : 'relative',
+                    top: 0,
+                    width: '100%',
+                    zIndex: 1000, // Ajusta según sea necesario
+                }}
+                dark
+                expand="md"
+            >
                 <NavbarToggler onClick={toggle} />
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="ms-auto" navbar>
